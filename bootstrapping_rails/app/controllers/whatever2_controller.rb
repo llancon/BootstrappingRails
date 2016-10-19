275f0b1b0b7cbdc67e429cb3b098f26093722915
llancon <rdzalb@gmail.com>
2016-10-19 15:40:57 -0400

    Added links to pages and bootstrap to give it a better look

--- bootstrapping_rails/app/controllers/whatever2_controller.rb
+++ bootstrapping_rails/app/controllers/whatever2_controller.rb
@@ -1,2 +1,5 @@
 class Whatever2Controller < ApplicationController
+  def giphy
+    @howdy = "Howdy human"
+  end
 end
--- bootstrapping_rails/config/routes.rb
+++ bootstrapping_rails/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get '/whatever' => 'whatever#hello'
+  get '/whatever' => 'whatever#hello', as: :human
+  get '/whatever2' => 'whatever2#giphy', as: :hello
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
